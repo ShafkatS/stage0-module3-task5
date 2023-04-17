0613461ac14a444206bff6c759563dfac654b835
ShafkatS <126591704+ShafkatS@users.noreply.github.com>
2023-04-17 16:26:50 +0500

    Update NumberDivider.java

--- src/main/java/lang/print/gaps/task5/NumberDivider.java
+++ src/main/java/lang/print/gaps/task5/NumberDivider.java
@@ -2,5 +2,7 @@ package lang.print.gaps.task5;
 
 public class NumberDivider {
     void divide(int toBeDivided) {
+        float numberAfterDivider = toBeDivided / 5F;
+        System.out.println(numberAfterDivider);
     }
 }
